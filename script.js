c06bad1b0eb59370d0bd40ac1280755da671f58c
Aaradhna Gurjar <aaradhnagurjarg@gmail.com>
2025-11-27 13:28:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,44 @@ function addToWishlist(id) {
 }
 
 
+
+function displayProducts(list) {
+    let html = "";
+    list.forEach(p => {
+        html += `
+            <div class="product-card">
+                <img src="${p.img}">
+                <h3>${p.name}</h3>
+                <p>₹${p.price}</p>
+                <p>⭐ ${p.rating}</p>
+                <button onclick="addToCart(${p.id})">Add to Cart</button>
+            </div>
+        `;
+    });
+
+    document.getElementById("products").innerHTML = html;
+}
+
+displayProducts(products);
+
+function filterProducts() {
+    let filtered = products;
+
+    let cat = document.getElementById("categoryFilter").value;
+    if (cat !== "all") filtered = filtered.filter(p => p.category === cat);
+
+    let rating = document.getElementById("ratingFilter").value;
+    filtered = filtered.filter(p => p.rating >= rating);
+
+    let search = document.getElementById("searchInput").value.toLowerCase();
+    filtered = filtered.filter(p => p.name.toLowerCase().includes(search));
+
+    let sort = document.getElementById("priceSort").value;
+    if (sort === "low") filtered.sort((a,b)=>a.price-b.price);
+    if (sort === "high") filtered.sort((a,b)=>b.price-a.price);
+
+    displayProducts(filtered);
+}
+
+document.querySelectorAll(".filters select, .filters input")
+.forEach(el => el.addEventListener("input", filterProducts));
