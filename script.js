b26bc8d9377cead0b38a41d7821586c8948d1ef8
Aaradhna Gurjar <aaradhnagurjarg@gmail.com>
2025-11-27 13:20:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 const emailField = document.getElementById("email-address-input");
 emailField.focus({
   preventScroll: true,
+
 });
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(id) {
+  const product = products.find(p => p.id === id);
+
+  const exist = cart.find(p => p.id === id);
+  if (exist) {
+    exist.qty++;
+  } else {
+    cart.push({...product, qty: 1});
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCartCount();
+}
+
+function updateCartCount() {
+  const count = cart.reduce((a, b) => a + b.qty, 0);
+  document.getElementById("cartCount").innerText = count;
+}
+
+updateCartCount();
